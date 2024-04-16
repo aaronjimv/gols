cf34ee47e00104ae0daca986f8485d7a3757337d
Aaron <aaronjimv@gmail.com>
2024-04-16 15:32:59 -0700

    add temple for icons and flags

--- ls_clone.go
+++ ls_clone.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 // Windows os system
 const Windows = "windows"
 
@@ -25,3 +27,30 @@ const (
 	jpg = ".jpg"
 	gif = ".gif"
 )
+
+type file struct {
+	name             string
+	fileType         int
+	isDir            bool
+	isHidden         bool
+	userName         string
+	groupName        string
+	size             int64
+	modificationTime time.Time
+	mode             string
+}
+
+type styleFileType struct {
+	symbol string
+	color  string
+	icon   string
+}
+
+var mapStyleByFileType = map[int]styleFileType{
+	fileRegular:    {icon: "📄"},
+	fileDirectory:  {icon: "📂", color: "color.FgBlue", symbol: "/"},
+	fileExecutable: {icon: "🚀", color: "color.FgGreen", symbol: "*"},
+	fileCompress:   {icon: "📦", color: "color.FgRed"},
+	fileImage:      {icon: "📸", color: "color.FgMagenta"},
+	fileLink:       {icon: "🔗", color: "color.FgCyan"},
+}
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "flag"
 
+func main() {
+	// filter flags
+	flagPattern := flag.String("p", "", "filter by pattern")
+	flagAll := flag.Bool("a", false, "all files including hide files")
+	flagNumberRecords := flag.Int("n", 0, "number of records")
+
+	// order flags
+	hasOrderByTime := flag.Bool("t", false, "sort by time, oldest first")
+	hasOrderBySize := flag.Bool("s", false, "sort by file size, smallest first")
+	hasOrderReverse := flag.Bool("r", false, "reverse order while sorting")
+
+	flag.Parse()
 }
